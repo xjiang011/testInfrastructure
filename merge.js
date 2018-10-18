c069c17bd2f333d26a38ec0e3e31919887177242
xjiang011 <xiujiang@thoughtworks.com>
2018-10-18 10:16:11 +0800

    Update merge.js

--- merge.js
+++ merge.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 var merge = require('merge-package-json');
 var src = fs.readFileSync('../localPackage.json');
-var dst = fs.readFileSync('./sharedPackage.json');
+var dst = Sync('./sharedPackage.json');
  
 // Create a new `package.json` 
 fs.writeFileSync('../package.json', merge(dst, src));
